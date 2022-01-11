84c0119d4a92febaab7ee4fa912c93e4ed6f7501
‘baolin <3071279398@qq.com>
2022-01-11 14:51:41 +0800

    登录

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,23 @@
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   // 配置选项
 })
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  // 我们就在这里通过改写 config 配置信息来实现业务功能的统一处理
+  const { user } = store.state
+  if (user && user.access_token) {
+    config.headers.Authorization = user.access_token
+  }
+  // 注意:这里一定要返回 config, 否则请求就发不出去了
+  return config
+}, function (err) {
+  return Promise.reject(err)
+})
 
-// 相应拦截器
+// 响应拦截器
 
 export default request
